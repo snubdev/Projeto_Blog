f20135d45950d350efffd251001a446e82934433
Maicon Leandro <snubhousedev@gmail.com>
2022-05-07 18:27:14 -0300

    Add função de paginação no projeto

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def post_list(request):
-    posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3) # três postagens em cada página
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # Se a página não for um inteiro, exibe a primeira página
+        posts = paginator.page(1)
+    except EmptyPage:
+        # Se a página estiver fora do intervalo
+        # exibe a última página de resultados
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
 
 
 def post_detail(request, year, month, day, post):
