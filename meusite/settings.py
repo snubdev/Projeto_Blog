bfc4fb070aa837c8eb1e4e8f8ca29fa68cad5341
Maicon Leandro <snubhousedev@gmail.com>
2022-06-07 00:29:25 -0300

    Add função de pesquisa de conteúdos do blog

--- meusite/settings.py
+++ meusite/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'taggit',
     'django.contrib.sites',
     'django.contrib.sitemaps',
+    'django.contrib.postgres',
+
 ]
 
 MIDDLEWARE = [
@@ -81,8 +83,12 @@ WSGI_APPLICATION = 'meusite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': ''
     }
 }
 
